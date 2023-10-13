5e61fc5126fbf1337f1f7cf08f452d7e3b4c03b7
David Delgado <davidd82@usc.edu>
2023-10-13 15:44:40 -0700

    Added light sensor check

--- test_suite.py
+++ test_suite.py
@@ -17,17 +17,34 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
 
-def ledBlink(x, board_num):
+def ledBlink(x, board_num, interval):
     for i in range(x):
         GPIO.output(board_num, GPIO.HIGH)
-        time.sleep(0.5)
+        time.sleep(interval)
         GPIO.output(board_num, GPIO.LOW)
-        time.sleep(0.5)
+        time.sleep(interval)
         i+=1
+
+def lightSensor(channel):
+    
+    for i in range (50):
+        light_val = mcp.read_adc(channel)
+        print(light_val)
+
+        if (light_val > 100):
+            print("bright")
+        else:
+            print("dark")
+        i+=1
+        time.sleep(0.1)
 
 # Main program loop.
 while True:
-    ledBlink(5, 11)
+    # LED blinks with 500ms intervals
+    ledBlink(5, 11, 0.5)
+
+    lightSensor(0)
+    
 
     # Read all the ADC channel values in a list.
     values = [0]*8
